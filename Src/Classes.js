c2ec34082f2d10395627983b0cfc667d91867f5f
Hud <hudalayubi@gmail.com>
2025-10-04 15:51:59 +0800

    Finished constructing rough draft of all classes with methods and properties

---
+++ Src/Classes.js
@@ -0,0 +1,34 @@
+class Ship {
+
+    shipType = {
+      "Aircraft Carrier" : arr.length = 5,
+      "Battleship" : arr.length = 4,
+      "Cruiser" : arr.length = 3,
+      "Submarine" : arr.length = 3,
+      "Destroyer" : arr.length = 2,
+    }
+
+    hit(i) {
+        return i++
+    };
+
+    isSunk() {
+        if (this.hit(i) === this.shipType) {
+            return "ship has sunked"
+        } else {
+            return "not sunk yet"
+        }
+    };
+}
+
+class Gameboard {
+    receiveAttack() {
+
+    }
+}
+
+class Player {
+    constructor() {
+
+    }
+}
